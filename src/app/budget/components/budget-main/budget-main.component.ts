d2739255ddc5fb5773eb2b85b90d0044ec403ccc
piolinest123 <jpablopm72@gmail.com>
2020-06-27 16:32:21 -0400

    Changed the font and worked on budget section

--- src/app/budget/components/budget-main/budget-main.component.ts
+++ src/app/budget/components/budget-main/budget-main.component.ts
@@ -10,12 +10,16 @@ export class BudgetMainComponent implements OnInit {
   current_month:string;
   current_year:number;
   info:DateInfo;
+  sign:boolean;
+  total_budget:number;
   constructor() { }
 
   ngOnInit(): void {
     this.info = new DateInfo();
     this.current_month = this.info.getMonthAsString();
     this.current_year = this.info.getYear();
+    this.sign = false;
+    this.total_budget = 1000;
   }
 
 }
