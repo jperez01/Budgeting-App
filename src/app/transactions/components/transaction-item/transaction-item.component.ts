d979c723625dc38e31a67d69482da26fcc7576fb
piolinest123 <jpablopm72@gmail.com>
2020-07-03 16:50:58 -0400

    Finished Up transactions list for app

--- src/app/transactions/components/transaction-item/transaction-item.component.ts
+++ src/app/transactions/components/transaction-item/transaction-item.component.ts
@@ -10,14 +10,75 @@ export class TransactionItemComponent implements OnInit {
   date_string:string;
   outflow:number;
   inflow:number;
+  changingValue:boolean;
+  background:string;
+  line:string;
+  sentItem:boolean;
+  newAccount:string;
+  newDate:string;
+  newCategory:string;
+  newDescription:string;
+  newInflow:number;
+  newOutflow:number;
+  styleChange:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.sentItem = false;
+    this.background = '';
+    this.line = '1px solid #b6bec2';
+    this.changingValue = false;
     this.date_string = this.getFormattedDate(this.item.date);
     this.outflow = this.item.outflow.toFixed(2);
     this.inflow = this.item.inflow.toFixed(2);
-    console.log(this.outflow);
-    console.log(this.inflow);
+    this.newAccount = this.item.account;
+    this.newCategory = this.item.category;
+    this.newDate = this.item.date;
+    this.newInflow = this.inflow;
+    this.newOutflow = this.outflow;
+    this.newDescription = this.item.description;
+  }
+
+  confirmChanges(): void {
+    if (this.newAccount !== undefined && this.newCategory !== undefined && this.newDate !== undefined
+      && this.newDescription !== undefined && this.newInflow !== undefined && this.newOutflow !== undefined) {
+        let newTransaction = {
+          account: this.newAccount,
+          date: new Date(this.newDate),
+          category: this.newCategory,
+          description: this.newDescription,
+          outflow: Number(this.newOutflow),
+          inflow: Number(this.newInflow)
+        }
+        this.newAccount = this.item.account;
+        this.newCategory = this.item.category;
+        this.newDate = this.item.date;
+        this.newInflow = this.inflow;
+        this.newOutflow = this.outflow;
+        this.newDescription = this.item.description;
+        this.changingValue = false;
+        this.background = '';
+        this.line = '1px solid #b6bec2';
+      } else {
+        console.log(this.newAccount);
+        console.log(this.newCategory);
+        console.log(this.newDate);
+        console.log(this.newOutflow);
+        console.log(this.newInflow);
+        console.log(typeof this.newOutflow);
+      }
+  }
+
+  cancelChange(): void {
+    this.changingValue = false;
+    this.background = '';
+    this.line = '1px solid #b6bec2';
+  }
+  changeStyle(): void {
+      this.background = '#0091d946';
+      this.line = 'none';
+      this.changingValue = true;
+      console.log('Outer');
   }
 
   getFormattedDate(date: Date): string {
@@ -30,5 +91,53 @@ export class TransactionItemComponent implements OnInit {
     day = day.length > 1 ? day : '0' + day;
     
     return month + '/' + day + '/' + year;
+  }
+
+  collectAccount(event: any): void {
+    if (event.target.value.localeCompare('') !== 0) {
+      this.newAccount = event.target.value;
+    } else {
+      this.newAccount = undefined;
+    }
+  }
+
+  collectDate(event: any): void {
+    if (event.target.value.localeCompare('') !== 0) {
+      this.newDate = event.target.value;
+    } else {
+      this.newDate = undefined;
+    }
+  }
+
+  collectCategory(event: any): void {
+    if (event.target.value.localeCompare('') !== 0) {
+      this.newCategory = event.target.value;
+    } else {
+      this.newCategory = undefined;
+    }
+  }
+
+  collectDescription(event: any): void {
+    if (event.target.value.localeCompare('') !== 0) {
+      this.newDescription = event.target.value;
+    } else {
+      this.newDescription = undefined;
+    }
+  }
+
+  collectOutflow(event: any): void {
+    if (!isNaN(event.target.value % 1)) {
+      this.newOutflow = event.target.value;
+    } else {
+      this.newOutflow = undefined;
+    }
+  }
+
+  collectInflow(event: any): void {
+    if (!isNaN(event.target.value % 1)) {
+      this.newInflow = event.target.value;
+    } else {
+      this.newInflow = undefined;
+    }
   }
 }
--- src/app/transactions/components/transactions-main/transactions-main.component.ts
+++ src/app/transactions/components/transactions-main/transactions-main.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionsMainComponent implements OnInit {
   transactions:Object[];
-  constructor() { }
+  addingTransaction:boolean;
+  newAccount:string;
+  newDate:string;
+  newCategory:string;
+  newDescription:string;
+  newInflow:number;
+  newOutflow:number;
+  constructor() {
+    this.newAccount = undefined;
+    this.newDate = undefined;
+    this.newCategory = undefined;
+    this.newDescription = undefined;
+    this.newInflow = undefined;
+    this.newOutflow = undefined;
+  }
 
   ngOnInit(): void {
+    this.addingTransaction = false;
     this.transactions = [
       {
         account: 'John Savings',
@@ -36,5 +51,82 @@ export class TransactionsMainComponent implements OnInit {
         inflow: 1000.00
       },
     ]
+  }
+
+  addNewTransaction(): void {
+    if (this.newAccount !== undefined && this.newCategory !== undefined && this.newDate !== undefined
+      && this.newDescription !== undefined && this.newInflow !== undefined && this.newOutflow !== undefined) {
+        let newTransaction = {
+          account: this.newAccount,
+          date: new Date(this.newDate),
+          category: this.newCategory,
+          description: this.newDescription,
+          outflow: Number(this.newOutflow),
+          inflow: Number(this.newInflow)
+        }
+        this.transactions.push(newTransaction);
+        this.addingTransaction = false;
+        this.newAccount = undefined;
+        this.newCategory = undefined;
+        this.newDate = undefined;
+        this.newInflow = undefined;
+        this.newOutflow = undefined;
+        this.newDescription = undefined;
+      } else {
+        console.log(this.newAccount);
+        console.log(this.newCategory);
+        console.log(this.newDate);
+        console.log(this.newOutflow);
+        console.log(this.newInflow);
+        console.log(typeof this.newOutflow);
+      }
+  }
+
+  collectAccount(event: any): void {
+    if (event.target.value.localeCompare('Account') !== 0 && event.target.value.localeCompare('') !== 0) {
+      this.newAccount = event.target.value;
+    } else {
+      this.newAccount = undefined;
+    }
+  }
+
+  collectDate(event: any): void {
+    if (event.target.value.localeCompare('06/20/01') !== 0 && event.target.value.localeCompare('') !== 0) {
+      this.newDate = event.target.value;
+    } else {
+      this.newDate = undefined;
+    }
+  }
+
+  collectCategory(event: any): void {
+    if (event.target.value.localeCompare('Category') !== 0 && event.target.value.localeCompare('') !== 0) {
+      this.newCategory = event.target.value;
+    } else {
+      this.newCategory = undefined;
+    }
+  }
+
+  collectDescription(event: any): void {
+    if (event.target.value.localeCompare('Description') !== 0 && event.target.value.localeCompare('') !== 0) {
+      this.newDescription = event.target.value;
+    } else {
+      this.newDescription = undefined;
+    }
+  }
+
+  collectOutflow(event: any): void {
+    if (!isNaN(event.target.value % 1)) {
+      this.newOutflow = event.target.value;
+    } else {
+      this.newOutflow = undefined;
+    }
+  }
+
+  collectInflow(event: any): void {
+    if (!isNaN(event.target.value % 1)) {
+      this.newInflow = event.target.value;
+    } else {
+      this.newInflow = undefined;
+    }
   }
 }
